36391e2158a5923fe595a3be07143e0361cce65f
PJ Hampton <pjhampton@protonmail.com>
2021-05-19 20:10:17 +0100

    Add some components.

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,5 +1,17 @@
+import Layout from '../components/Layout';
+
 const Index = ({ title, description, ...props }) => {
-  return <div>Hello, world!</div>
+  return (
+    <Layout pageTitle={title}>
+      <h1 className="title">Welcome to my blog!</h1>
+      <p className="description">
+        {description}
+      </p>
+      <main>
+        Posts go here
+      </main>
+    </Layout>
+  )
 }
 
 export default Index
