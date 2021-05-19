edc6a09ca81287d1f4c64d1d768acd78d7a11070
PJ Hampton <pjhampton@protonmail.com>
2021-05-19 19:55:31 +0100

    Add blog scaffold.

---
+++ blog/pages/index.js
@@ -0,0 +1,16 @@
+const Index = ({ title, description, ...props }) => {
+  return <div>Hello, world!</div>
+}
+
+export default Index
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
+}
