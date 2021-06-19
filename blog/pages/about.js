68a2798d0719e20aed7d6d3c60c19ef115ed6770
PJ Hampton <pjhampton@protonmail.com>
2021-06-19 22:24:44 +0100

    Add readme

--- blog/pages/about.js
+++ blog/pages/about.js
@@ -6,13 +6,22 @@ const About = ({ title, description, ...props }) => {
       <Layout pageTitle={`${title} | About`} description={description}>
         <h1 className="title">About</h1>
 
-        <p className="description">
-          {description}
-        </p>
+        <div className="content">
+          <p>
+            I am a computer scientist from Belfast, N. Ireland. Although my heart is in the
+            JVM you will find me wrangling Go, C++, Python and writing copious amounts of JavaScript. 
+            I read my PhD in Artifical Intelligence at AIARG, Ulster University under the
+            supervision of Hui Wang, Zhiwei Lin, and William Blackburn. I'm particulary interested
+            in stream processing and distributed systems in the context of data intensive applications.
+          </p>
 
-        <p>
-          I am a very exciting person.
-        </p>
+          <p>
+            When not wrestling with computers, you will find me trying to be a present husband and doggy dad,
+            meditating, practicing yoga, reading, and working on my garden. I'm <strong>not</strong> currently available for 
+            private work or co-founding new projects but feel free to connect with me. If my work has helped 
+            you or your company please consider buying me a coffee sometime or more importantly - pay it forward.
+          </p>
+        </div>
       </Layout>
     </>
   )
