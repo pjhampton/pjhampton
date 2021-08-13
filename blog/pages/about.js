19b7f633db04ecf06c236cd97a2c86220d6803f9
Pete Hampton <pjhampton@users.noreply.github.com>
2021-08-13 14:13:53 +0100

    Update about.js

--- blog/pages/about.js
+++ blog/pages/about.js
@@ -9,16 +9,15 @@ const About = ({ title, description, ...props }) => {
         <div className="content">
           <p>
             I am a computer scientist from Belfast, N. Ireland. Although my heart is in the
-            JVM you will find me wrangling Go, C++, Python and writing copious amounts of JavaScript. 
+            JVM you will find me wrangling C++, Go and writing copious amounts of JavaScript. 
             I read my PhD in Artifical Intelligence at AIARG, Ulster University under the
             supervision of Hui Wang, Zhiwei Lin, and William Blackburn. I'm particulary interested
-            in search problems, security, stream processing, and distributed systems in the context of data intensive applications.
+            in application security, stream processing, and distributed systems in the context of data intensive applications.
           </p>
 
           <p>
             When not wrestling with computers, you will find me trying to be a present husband and doggy dad,
-            meditating, practicing yoga, reading, and working on my garden. I'm <strong>not</strong> currently available for 
-            private work or co-founding new projects but feel free to connect with me. If my work has helped 
+            meditating, practicing yoga, reading, and working on my garden. If my work has helped 
             you or your company please consider buying me a coffee sometime or more importantly - <i>pay it forward</i>.
           </p>
         </div>
