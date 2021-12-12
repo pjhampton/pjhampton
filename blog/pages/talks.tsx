3f04e29087e042cb1917cbfa5cdb765d13094045
PJ Hampton <pjhampton@protonmail.com>
2021-12-12 12:14:59 +0000

    update date on talks.

--- blog/pages/talks.tsx
+++ blog/pages/talks.tsx
@@ -12,7 +12,7 @@ export default function Talks({ title } : TalksProps) {
         <h1>Talks</h1>
 
         <ul>
-          <li>2021-10-06 (<strong>ElasticON 21</strong>) - Data Streams & Pipelines: <i>Observing with the Elastic Stack</i>. <a href="/talks/pjhampton-elasticon-oct2021.pdf" target="_blank">[Slides]</a></li>
+          <li>2021-Nov-06 (<strong>ElasticON 21</strong>) - Data Streams & Pipelines: <i>Observing with the Elastic Stack</i>. <a href="/talks/pjhampton-elasticon-oct2021.pdf" target="_blank">[Slides]</a></li>
         </ul>
       </>
     </Layout>
