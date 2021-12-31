e89b1363a64f439d6728b7b731c979e6c9b22078
PJ Hampton <pjhampton@protonmail.com>
2021-12-31 22:32:33 +0000

    don't wrap codelines.

--- blog/components/Components.tsx
+++ blog/components/Components.tsx
@@ -9,7 +9,7 @@ const components = {
   code({node, inline, className, children, ...props}: CodeProps) {
     const match = /language-(\w+)/.exec(className || '')
     return !inline && match ? (
-      <SyntaxHighlighter style={atomDark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+      <SyntaxHighlighter wrapLines={false} style={atomDark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
     ) : (
       <code className={className} {...props} />
     )
