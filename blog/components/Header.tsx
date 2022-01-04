e45a34fe301e332819b193124413236b60443191
Pete Hampton <peter.hampton@elastic.co>
2022-01-04 18:32:06 +0000

    Add blur effect to avatar.

--- blog/components/Header.tsx
+++ blog/components/Header.tsx
@@ -15,6 +15,7 @@ export default function Header() {
               className="avatar"
               height={150}
               width={150}
+              placeholder="blur"
               src={avatar} />
           </a>
         </Link>
