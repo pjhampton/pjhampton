ba1e864ac3f3038560e1a91e0b348e03572aadf3
Pete Hampton <pjhampton@users.noreply.github.com>
2023-02-04 17:58:04 +0000

    Fix broken type.

--- blog/components/CodeContainer.tsx
+++ blog/components/CodeContainer.tsx
@@ -1,11 +1,18 @@
-import { CodeComponent } from 'react-markdown/src/ast-to-react'
+import type { CodeComponent, ReactMarkdownProps } from 'react-markdown/lib/ast-to-react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
-type CodeProps = Parameters<CodeComponent>[0]
+type CodeComponentProps = JSX.IntrinsicElements['code'] &
+  ReactMarkdownProps & {
+    inline?: boolean;
+  };
+
+  interface CodeProps extends Omit<CodeComponentProps, 'ref'> {
+  codeTagProps?: any;
+}
 
 const CodeContainer = {
-  code({node, inline, className, children, ...props}: CodeProps) {
+  code({node, className, inline, children, ...props}: CodeProps) {
     const match = /language-(\w+)/.exec(className || '')
     return !inline && match ? (
       <SyntaxHighlighter
