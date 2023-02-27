baf0ec8abfc1ca268360e5e9763f5aa8e2de91bc
Pete Hampton <pjhampton@users.noreply.github.com>
2023-02-27 21:30:01 +0000

    Update.

--- blog/components/ProfileCard.tsx
+++ blog/components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import avatar from '@public/avatar.png'
+import avatar from '@public/avatar.webp'
 import SocialIcons from './SocialIcons'
 import SiteImage from '@components/SiteImage'
 import ThemeToggle from '@components/ThemeToggle'
