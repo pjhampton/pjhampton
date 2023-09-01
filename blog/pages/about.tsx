a92b90234fc770ebdb161d952eadb06e3f275a8e
Pete Hampton <pjhampton@users.noreply.github.com>
2023-09-01 20:47:12 +0100

    Deploy new about.

--- blog/pages/about.tsx
+++ blog/pages/about.tsx
@@ -15,16 +15,16 @@ export default function About() {
           </h1>
 
           <p>
-            I&apos;m a Software Developer from Belfast, N. Ireland. My expertise is in Java, but you 
+            My expertise is in Java, but you 
             will also find me programming C++, Go, and TypeScript. I specialise in software design and 
             distributed systems in the context of data intensive applications. I also enjoy the Linux and SysAdmin world.
             When not wrangling computers for a living, you will find me working to be a present father and husband,
-            meditating, reading, and practicing yoga. I also collect guitar pedals and enjoy learning about
+            meditating, reading, and practicing yoga. I collect guitar pedals and enjoy learning about
             Digital Signal Processing.
           </p>
 
           <p>
-            If my work or content has helped you or your business consider paying it forward somehow.
+            If my work or content has helped you or your business consider paying it forward.
           </p>
         </>
       </Layout>
