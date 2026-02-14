b0ff79669c5ac760cbfcca87385da3e50f2197d9
Pete Hampton <pjhampton@users.noreply.github.com>
2026-02-14 11:43:37 +0000

    fix(blog): regression to browser date handling

--- blog/src/utils/datetime.ts
+++ blog/src/utils/datetime.ts
@@ -1,1 +1,9 @@
-export const formatDate = (date: string) => date.split('-').reverse().join(' ');
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const formatDate = (date: string) => {
+  const d = new Date(date);
+  const day = d.getUTCDate();
+  const month = MONTHS[d.getUTCMonth()];
+  const year = d.getUTCFullYear();
+  return `${day} ${month} ${year}`;
+};
